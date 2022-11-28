368ed9c906ca9c209ac7d8b9e3b6b750f6ea9771
jiaezzang <visveryver2@gmail.com>
2022-11-28 19:06:28 +0900

    자식 엘리먼트 추가

--- React-quickly/introduce/jsx/script.jsx
+++ React-quickly/introduce/jsx/script.jsx
@@ -4,6 +4,15 @@ ReactDOM.render(
             <h1>JIAEZZANG</h1>
             <p>Helllo, Everyone! I'm JIAEzzang.</p>
         </Content>
+        <Content>
+            <img src="image/me.jpg" width="100"/>
+        </Content>
+        <Content>
+            <a href="https://jiaezzang.github.io">https://jiaezzang.github.io</a>
+        </Content>
+        <Content>
+            <a className="btn btn-danger" href="https://github.com/jiaezzang">https://github.com/jiaezzang</a>
+        </Content>
     </div>,
     document.getElementById('content')
 )
