c3be175978ee878fff9b9038daa02ef66d00a911
jiaezzang <visveryver2@gmail.com>
2022-11-28 19:01:51 +0900

    소개 페이지 기본 구조 생성

---
+++ React-quickly/introduce/jsx/content.jsx
@@ -0,0 +1,7 @@
+class content extends React.Component { 
+        render() {
+            return <div className="content">
+                {this.props.children}
+            </div>
+    }
+}
---
+++ React-quickly/introduce/jsx/script.jsx
@@ -0,0 +1,9 @@
+ReactDOM.render(
+    <div>
+        <Content>
+            <h1>JIAEZZANG</h1>
+            <p>Helllo, Everyone! I'm JIAEzzang.</p>
+        </Content>
+    </div>,
+    document.getElementById('content')
+)
