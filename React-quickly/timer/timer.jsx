ce05351fc08c639bf9ecdadfff4d6456fa81f181
jiaezzang <visveryver2@gmail.com>
2022-11-29 18:42:32 +0900

    TimerWrapper 컴포넌트 생성

--- React-quickly/timer/timer.jsx
+++ React-quickly/timer/timer.jsx
@@ -1,12 +1,33 @@
 class TimerWrapper extends React.Component {
     constructor(props) {
-        ...
+        super(props)
+        this.state = {timeLeft: null, timer: null}
+        this.startTimer = this.startTimer.bind(this)
     }
     startTimer(timeLeft){
-        ...
+        clearInterval(this.state.timer)
+        let timer = setInterval(() => {
+            console.log('2: Inside of setInterval')
+            var timeLeft = this.state.timeLeft -1
+            if (timeLeft == 0) clearInterval(timer)
+            this.setState({timeLeft: timeLeft, timer: timer})
+        }, 1000)
+        console.log('1: After setInterval')
+        return this.setState({timeLeft: timeLeft, timer: timer})
     }
     render() {
-        ...
+        return (
+            <div className="row-fluid">
+                <h2>Timer</h2>
+                <div className="btn-group" role="group">
+                    <button time="5" startTimer={this.startTimer} />
+                    <button time="10" startTimer={this.startTimer} />
+                    <button time="15" startTimer={this.startTimer} />
+                </div>
+                <Timer timeLeft={this.state.timeLeft} />
+                <audio id="end-of-time" src="flute_c_long_01.wav" preload="auto"></audio>
+            </div>
+        )
     }
 }
 
