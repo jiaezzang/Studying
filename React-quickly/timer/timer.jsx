de3523c2c6f602e8fbdc7081128370196ad616d8
jiaezzang <visveryver2@gmail.com>
2022-11-30 17:14:10 +0900

    button 컴포넌트 생성

--- React-quickly/timer/timer.jsx
+++ React-quickly/timer/timer.jsx
@@ -44,10 +44,13 @@ class Timer extends React.Component {
 
 class Button extends React.Component {
     startTimer(event) {
-        ...
+        return this.props.startTimer(this.props.time)
     }
     rneder() {
-        ...
+        return <button type="button" className="btn-default btn"
+        onClick={this.startTimer.bind(this)}>
+            {this.props.time} seconds
+        </button>
     }
 }
 
