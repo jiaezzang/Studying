315f2d4c5c09dd4a9db9e53a777990cff35a2633
jiaezzang <visveryver2@gmail.com>
2022-12-07 15:37:26 +0900

    forecast 컴포넌트 작성

--- react-native/WeatherProject/Forecast.js
+++ react-native/WeatherProject/Forecast.js
@@ -1,2 +1,42 @@
-import WeatherProject from "./WeatherProject"
-export default WeatherProject;
+import React, { Component } from "react";
+
+import { StyleSheet, Text, View } from "react-native";
+
+class forecast extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.bigText}>
+                    {this.props.main}
+                </Text>
+                <Text style={styles.mainText}>
+                    Current conditions: {this.props.description}
+                </Text>
+                <Text style={styles.bigText}>
+                    {this.props.temp}°F
+                </Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: { 
+        height: 130 
+    },
+    bigText: {
+        flex: 2,
+        fontSize: 20,
+        textAlign: "center",
+        margin: 10,
+        color: "#FFFFF"
+    },
+    mainText: {
+        flex: 1,
+        fontSize: 16,
+        textAlign: "center",
+        color: "#FFFFF"
+    }
+});
+
+export default forecast;
