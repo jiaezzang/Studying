f112b681003ac84b46db1469f2ed93d54246781f
jiaezzang <visveryver2@gmail.com>
2022-12-08 10:53:28 +0900

    이미지 추가

--- react-native/WeatherProject/WeatherProject.js
+++ react-native/WeatherProject/WeatherProject.js
@@ -1,21 +1,21 @@
 import React, { Component } from "react";
 
-import { StyleSheet, Text, View, TextInput } from "react-native";
+import { StyleSheet, Text, View, TextInput, ImageBackground } from "react-native";
+
 import Forecast from "./Forecast";
 import OpenWeatherMap from "./open_weather_map";
 
 class WeatherProject extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
-    this.state = { zip:"", forecast: null };
+    this.state = { zip: "", forecast: null };
   }
 
   _handleTextChange = event => {
     let zip = event.nativeEvent.text;
     OpenWeatherMap.fetchForecast(zip).then(forecast => {
-      console.log(forecast);
       this.setState({ forecast: forecast });
-    })
+    });
   };
 
   render() {
@@ -30,38 +30,73 @@ class WeatherProject extends Component {
       );
     }
     return (
-      <View style={StyleSheet.container}>
-        <Text style={StyleSheet.welcome}>
-          you input {this.state.zip}.
-        </Text>
-        <TextInput 
-          style={StyleSheet.input}
-          onSubmitEditing={this._handleTextChange}
-        />
+      <View style={styles.container}>
+        <ImageBackground
+          source={require("./flowers.png")}
+          resizeMode="cover"
+          style={styles.backdrop}
+        >
+          <View style={styles.overlay}>
+            <View style={styles.row}>
+              <Text style={styles.mainText}>
+                Current weather for
+              </Text>
+              <View style={styles.zipContainer}>
+                <TextInput
+                  style={[styles.zipCode, styles.mainText]}
+                  onSubmitEditing={this._handleTextChange}
+                  underlineColorAndroid="transparent"
+                />
+              </View>
+            </View>
+            {content}
+          </View>
+        </ImageBackground>
       </View>
     );
   }
 }
 
+const baseFontSize = 16;
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#666666"
+  container: { 
+    flex: 1, 
+    alignItems: "center", 
+    paddingTop: 30 
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
+  backdrop: { flex: 1, 
+    flexDirection: "column" 
   },
-  input: {
-    fontSize: 20,
-    borderWidth: 2,
-    padding: 2,
-    height: 40,
-    width: 100,
-    textAlign: "center"
+  overlay: {
+    paddingTop: 5,
+    backgroundColor: "#000000",
+    opacity: 0.5,
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  row: {
+    flexDirection: "row",
+    flexWrap: "nowrap",
+    alignItems: "flex-start",
+    padding: 30
+  },
+  zipContainer: {
+    height: baseFontSize + 10,
+    borderBottomColor: "#DDDDDD",
+    borderBottomWidth: 1,
+    marginLeft: 5,
+    marginTop: 3
+  },
+  zipCode: { 
+    flex: 1, 
+    flexBasis: 1, 
+    width: 50, 
+    height: baseFontSize 
+  },
+  mainText: { 
+    fontSize: baseFontSize, 
+    color: "#FFFFFF" 
   }
 });
 
