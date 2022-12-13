170412f22cd4b7811c684ef48ed59cc3198ebcf9
jiaezzang <visveryver2@gmail.com>
2022-12-13 14:26:36 +0900

    연산 문제풀이

---
+++ programmers/lv0/arithmetic.js
@@ -0,0 +1,29 @@
+// num1에서 num2를 뺀 수 구하기
+function solution(num1, num2) {
+    var answer = 0;
+    if(-50000<=num1<=50000 && -50000<=num2<=50000){
+        answer = num1 - num2;
+    }
+    return answer;
+}
+//화살표 함수로 간단하게 표현
+// const solution = (num1, num2) => num1 - num2
+
+
+// num1과 num2를 곱한 수 구하기
+function solution(num1, num2) {
+    0<=num1<=100;
+    0<=num2<=100;
+    return num1 * num2;
+}
+//화살표 함수로 간단하게 표현
+// const solution = (num1, num2) => num1 * num2
+
+
+// num1로 num2를 나눈 값의 나머지 구하기
+function solution(num1, num2) {
+    return num1%num2;
+}
+// 화살표 함수로 간단하게 표현
+// const solution = (num1, num2) => num1 % num2
+
