d012634e4a0e0241fb0027680080e5ef33223d00
jiaezzang <visveryver2@gmail.com>
2023-01-19 00:14:52 +0900

    영어가 싫어요 문제 풀이

--- programmers/lv0/hateEng.js
+++ programmers/lv0/hateEng.js
@@ -9,3 +9,16 @@ function solution(numbers) {
     return Number(numbers);
 }
 
+//다른 사람의 풀이
+function solution(numbers) {
+    const obj = {
+        zero: 0, one: 1, two: 2, three: 3, four: 4,
+        five: 5, six: 6, seven: 7, eight: 8, nine: 9
+    };
+
+    const num = numbers.replace(/zero|one|two|three|four|five|six|seven|eight|nine/g, (v) => {      //replace 메서드를 사용하려면 정규식 활용
+        return obj[v];
+    });
+
+    return Number(num);
+}
