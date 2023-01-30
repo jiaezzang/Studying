b8e5fb7e3c945bf79b194b089cc064fad18389fa
jiaezzang <visveryver2@gmail.com>
2023-01-30 20:36:33 +0900

    create  routes

--- react-for-beginners/moveie-app/src/App.js
+++ react-for-beginners/moveie-app/src/App.js
@@ -1,7 +1,18 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+import Home from "./routes/Home";
 function App() {
-  return (
-    <div></div>
-  );
+ return <Router>
+  <Switch>
+    <Route path="/">
+      <Home />
+    </Route>
+  </Switch>
+ </Router>
 }
 
 export default App;
